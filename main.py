25a1f5ba9a4b151790afbbd89550df2e64f558fb
Konstantin <kamagoroff@gmail.com>
2023-03-29 01:55:57 +0300

    Updated

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 HELP = """
-help - напечатать справку по программе.
-add/todo - добавить задачу в список (название задачи запрашиваем у пользователя).
-print/show - напечатать все добавленные задачи."""
+help - Напечатать справку по программе.
+add/todo - Добавить задачу в список.
+print/show - Напечатать все задачи.
+random - Добавить случайную задачу на сегодня."""
 
-tasks = []
+RANDOM_TASK = "Поработать с кодом."
+tasks = {}
 today = []
-tomorrow = []
-other = {}
+# tomorrow = []
+# other = {}
+
+def appendInDict(date, task):
+  if date in tasks:
+      tasks[date].append(task)
+  else: 
+      tasks[date] = [task]
+  print("Добавлена задача:", task, "на дату:", date)
+  return tasks
 
 while(True):
   command = input("Введите команду: ")
@@ -15,18 +25,36 @@ while(True):
   elif command == "add" or command == "todo":
     task = input("Введите название задачи: ")
     date = input("Введите дату выполнения задачи: ")
-    if date == "today":
-      today.append(task)
-    elif date == "tomorrow":
-      tomorrow.append(task)
-    else:
-      other[task] = date
-    # tasks.append(task)
-    print("Добавлена задача:", task, "на дату:", date)
+    
+    # if date in tasks:
+    #   tasks[date].append(task)
+    # else: 
+    #   tasks[date] = [task]
+    appendInDict(date, task)
+    
+    # if date == "today":
+    #   today.append(task)
+    # elif date == "tomorrow":
+    #   tomorrow.append(task)
+    # else:
+    #   other[task] = date
+    
   elif command == "exit":
     print("Работа программы завершена.")
     break
   elif command == "show" or command == "print":
-    print("Задачи на сегодня:", today, "Задачи на завтра:", tomorrow, "Другие задачи:", other)
+    # print(tasks, "Задачи на сегодня:", today, "Задачи на завтра:", tomorrow, "Другие задачи:", other)
+    date = input("Введите дату для отображения списка задач: ")
+    if date in tasks:
+      for task in tasks[date]:
+        print(date, "-", task)
+    else:
+      print("Такой даты нет в задачах!")
+  elif command == "random":
+    # if "today" in tasks:
+    #   tasks["today"].append(RANDOM_TASK)
+    # else:
+    #   tasks["today"] = [RANDOM_TASK]
+    appendInDict("today", RANDOM_TASK)
   else:
    print("Нет такой команды!")
